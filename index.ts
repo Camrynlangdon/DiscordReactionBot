44aff096c02a31834aaa170c6611bf41e2274f6a
Camrynlangdon <44718126+Camrynlangdon@users.noreply.github.com>
2022-05-04 09:04:56 -0700

    discord bot

---
+++ index.ts
@@ -0,0 +1,89 @@
+import DiscordJS, {Client, Intents, Message, MessageEmbed, User } from 'discord.js';
+
+//////////////////////////////////////////////////////////////////
+
+// CONFIG VALUES
+const Token = "insert token here";  // This is a super secret TOKEN or Password, do not show anyone this for any reason
+const adminID = `169289644494684161` // This is the ID of the user it will DM, note: user has to be in the same serer as the bot to work
+const adminReaction = '🎈'; // The bot will run when this emoji as been placed
+const emojiToCheck = '👍';  // The bot will check who has reacted to this specific emoji
+
+
+//////////////////////////////////////////////////////////////////
+
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        
+    ],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+})
+
+client.on('ready', () => {
+    console.log('The bot is running')
+})
+
+
+client.on('messageCreate', async (message) => {
+    if(message.content === '/test/bot'){
+        message.reply({
+            content: 'working',
+        })
+    }
+    if(message.content === '/test/bot/inbox'){
+        message.reply({
+            content: 'Sending test DM',
+        })
+        const userToSendInfoTo = client?.users?.cache?.get(adminID);
+        userToSendInfoTo?.send(`If you are receiving this message, the bot is working correctly`);
+    }
+})
+
+
+client.on('messageReactionAdd', async (reaction: any, user: any) => {
+    if(reaction._emoji.name != adminReaction) return;
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+			return;
+		}
+	}
+
+
+	console.log(`reading from emoji  ${emojiToCheck}`);
+	console.log(`Post made by: ${reaction.message.author.username} #${reaction.message.author.discriminator}`);
+    console.log(`Post reads: ${reaction.message.content}`);
+
+    const messageReaction = reaction.message?.reactions.cache.get(emojiToCheck);
+    const users = await messageReaction?.users.fetch();
+    const userMap = users.map(((user: any) => "  " + user.username + " #" + user.discriminator));
+    console.log("users that reacted ", userMap.length);
+    
+    console.log("");
+    console.log("Users that reacted")
+    for(let i = 0; i < userMap.length; i++){
+        console.log(`${userMap[i]}`);
+    }
+
+    //DM admin the results
+    const userToSendInfoTo = client?.users?.cache?.get(adminID);
+    userToSendInfoTo?.send(`reading from emoji  ${emojiToCheck}`);
+    userToSendInfoTo?.send(`Post made by: ${reaction.message.author.username} #${reaction.message.author.discriminator}`);
+    userToSendInfoTo?.send(`Post reads: ${reaction.message.content}`);
+    userToSendInfoTo?.send(`Number of users that reacted: ${userMap.length}`);
+    userToSendInfoTo?.send(`Users that reacted: ${userMap}`);
+    console.log(`Sent DM to admin's inbox containing the bot's response`);
+});
+
+
+
+
+client.login(Token)
+
+
+
---
+++ readme.txt
@@ -0,0 +1,25 @@
+To start the bot: 
+
+1. Navigate to the correct file
+    open 'command prompt' from start-menu
+    cd desktop
+    cd ReactionBot
+
+2. Make sure you have the correct dependencies installed, Note: you only have to do this once
+    Install Node.js : https://nodejs.org/en/download/
+    Install the code dependencies by running command: npm install  //Note: must be within the bots directory > see #1
+
+3. Start the bot 
+    run command: ts-node index.ts
+
+4. To stop the bot or to reset the bot:
+    focus the command prompt window and 'ctl-c' then 'Y'
+    see #3 to start the bot back up
+
+5. To Edit Config, open index.tx in any text editor and change values under 'CONFIG VALUES'
+    Token: This is a super secret TOKEN or Password, do not show anyone this for any reason
+    adminID: This is the ID of the user it will DM, note: user has to be in the same serer as the bot to work
+    adminReaction: The bot will run when this emoji as been placed.
+    emojiToCheck: The bot will check who has reacted to this specific emoji
+
+NOTE: I suggest limiting the reaction emojis to a single emoji so the bot will be able to read from a single reference point and users won't be able to trigger the bot using the trigger emoji.
